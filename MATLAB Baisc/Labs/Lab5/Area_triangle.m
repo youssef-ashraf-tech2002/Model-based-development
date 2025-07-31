dafd08cc2eb86454dbf37c8533936eb25041b6ee
youssef-ashraf-tech2002 <ashraffy943@gmail.com>
2025-08-01 02:25:31 +0300

    Update Area_triangle.m

--- MATLAB Baisc/Labs/Lab5/Area_triangle.m
+++ MATLAB Baisc/Labs/Lab5/Area_triangle.m
@@ -1,5 +1,4 @@
 % Task 6: Anonymous Functions
-% 
 % Create an anonymous function that calculates the area of a triangle. The function should take base and height as inputs.
 % Call the anonymous function with different base and height values to calculate triangle areas.
 
